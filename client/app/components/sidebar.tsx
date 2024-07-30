81c20b610330f097ec6ebde7d90c47ab3a283c8c
SorrawitS <sangmanee773@gmail.com>
2024-07-30 15:01:41 +0700

    test

--- client/app/components/sidebar.tsx
+++ client/app/components/sidebar.tsx
@@ -1,12 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
 function SideBar() {
-
-  useEffect(() => {
-    
-  })
-
-  
   return (
     <div className='h-screen bg-red-200 w-[250px]'>
         SideBar
